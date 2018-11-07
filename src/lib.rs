5ac856c7220c25cff941f47d2c834028b18f32bf
Shinichi Kogai <kogai0121@gmail.com>
2018-11-08 00:12:57 +0900

    :recycle: Use byte module

--- src/lib.rs
+++ src/lib.rs
@@ -26,15 +26,6 @@ enum ValueType {
     // F63,
 }
 
-impl ValueType {
-    fn from_byte(code: u8) -> Self {
-        match code {
-            0x7f => ValueType::I32,
-            _ => unreachable!(),
-        }
-    }
-}
-
 #[derive(Debug, PartialEq)]
 struct Module {
     types: Vec<FnType>,
@@ -56,8 +47,7 @@ impl Store {
 
 #[derive(Debug)]
 pub struct Vm {
-    bytes: Vec<u8>,
-    len: usize,
+    bytes: byte::Byte,
     bp: usize,
     store: Store,
     stack: VecDeque<Value>,
@@ -67,8 +57,7 @@ pub struct Vm {
 impl Vm {
     pub fn new(bytes: Vec<u8>) -> Self {
         Vm {
-            len: bytes.len(),
-            bytes,
+            bytes: byte::Byte::new(bytes),
             bp: 0,
             store: Store::new(),
             stack: VecDeque::new(),
@@ -79,99 +68,8 @@ impl Vm {
         }
     }
 
-    fn has_next(&self) -> bool {
-        self.bp < self.len
-    }
-
-    fn next(&mut self) -> Option<&u8> {
-        let el = self.bytes.get(self.bp);
-        self.bp += 1;
-        el
-    }
-
-    fn decode_section(&mut self) {
-        match self.next() {
-            Some(0x1) => {
-                // Type Section
-                let &size_of_section = self.next().unwrap();
-                let &_num_of_type = self.next().unwrap();
-                // FIXME: Should iterate over num_of_type
-                match self.next() {
-                    Some(0x60) => {
-                        let &_num_of_param = self.next().unwrap();
-                        let &_num_of_result = self.next().unwrap();
-                        let &result_type = self.next().unwrap();
-                        self.module
-                            .types
-                            .push((vec![], vec![ValueType::from_byte(result_type)]));
-                    }
-                    _ => {}
-                }
-                println!("Hit type section, consist of {:?} bytes", &size_of_section);
-            }
-            Some(0x3) => {
-                // Function section
-                let &size_of_section = self.next().unwrap();
-                // FIXME: Should iterate over num_of_type_idx
-                let &_num_of_type_idx = self.next().unwrap();
-                let &_type_idx = self.next().unwrap();
-                println!(
-                    "Hit function section, consist of {:?} bytes",
-                    &size_of_section
-                );
-            }
-            Some(0x7) => {
-                // Export section
-                let &size_of_section = self.next().unwrap();
-                // FIXME: Should iterate over num_of_export
-                let &_num_of_export = self.next().unwrap();
-                let &_num_of_name = self.next().unwrap();
-                let mut buf = vec![];
-                for _ in 0.._num_of_name {
-                    let &el = self.next().unwrap();
-                    buf.push(el);
-                }
-
-                let &_export_description = self.next().unwrap(); // == 0x0
-                let &_function_idx = self.next().unwrap();
-                println!(
-                    "Hit export section, consist of {:?} bytes. Function named {:?}",
-                    &size_of_section,
-                    String::from_utf8(buf).unwrap()
-                );
-            }
-            Some(0xa) => {
-                // Code section
-                let &size_of_section = self.next().unwrap();
-                // FIXME: Should iterate over num_of_code
-                let &_num_of_code = self.next().unwrap();
-                let &_size_of_function = self.next().unwrap();
-                let &_num_of_param = self.next().unwrap();
-                match self.next() {
-                    Some(0x41) => {
-                        // FIXME: Decode expressions properly.
-                        // This implementation may don't make any sense.
-                        let &v = self.next().unwrap();
-                        let idx = self.module.func_addresses.len();
-                        self.stack.push_front(Value::I32(v as i32));
-                        self.module.func_addresses.push(idx as i32);
-                        let body = vec![Op::Const(v as i32)];
-                        self.store.function_instance.push(FunctionInstance { body });
-                    }
-                    Some(_) | None => unimplemented!(),
-                }
-                let &_end = self.next().unwrap();
-                println!("Hit code section, consist of {:?} bytes.", &size_of_section);
-            }
-            Some(_) => unimplemented!(),
-            None => {}
-        }
-    }
-
     pub fn decode(&mut self) {
-        while self.has_next() {
-            self.decode_section();
-        }
+        let _ = self.bytes.decode();
     }
 
     pub fn run(&mut self) -> Value {
