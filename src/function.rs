97419fcdae100b1a998787e352e716f91a2e6bc1
Shinichi Kogai <kogai0121@gmail.com>
2018-12-09 15:59:01 +0900

    :construction: Prepare to implement all type checking

--- src/function.rs
+++ src/function.rs
@@ -84,11 +84,182 @@ impl FunctionInstance {
     while inst_ptr < instructions.len() {
       // NOTE: Peek next
       if let Some(End) = instructions.get(inst_ptr + 1) {
-        let instruction = instructions.get(inst_ptr);
+        let instruction = instructions.get(inst_ptr)?;
         match instruction {
-          Some(I32Const(_)) => return_types.push(ValueTypes::I32),
-          Some(I64Const(_)) => return_types.push(ValueTypes::I64),
-          _ => { /* Nop */ }
+          I32Const(_)
+          | I32CountLeadingZero
+          | I32CountTrailingZero
+          | I32CountNonZero
+          | I32Add
+          | I32Sub
+          | I32Mul
+          | I32DivSign
+          | I32DivUnsign
+          | I32RemSign
+          | I32RemUnsign
+          | I32And
+          | I32Or
+          | I32Xor
+          | I32ShiftLeft
+          | I32ShiftRIghtSign
+          | I32ShiftRightUnsign
+          | I32RotateLeft
+          | I32RotateRight
+          | I32EqualZero
+          | Equal
+          | NotEqual
+          | LessThanSign
+          | LessThanUnsign
+          | I32GreaterThanSign
+          | I32GreaterThanUnsign
+          | I32LessEqualSign
+          | I32LessEqualUnsign
+          | I32GreaterEqualSign
+          | I32GreaterEqualUnsign
+          | I32Load(_, _)
+          | I32Load8Sign(_, _)
+          | I32Load8Unsign(_, _)
+          | I32Load16Sign(_, _)
+          | I32Load16Unsign(_, _)
+          | MemorySize
+          | MemoryGrow
+          | I32WrapI64
+          | I32TruncUnsignF64
+          | I32ReinterpretF32 => return_types.push(ValueTypes::I32),
+          I64Const(_)
+          | I64CountLeadingZero
+          | I64CountTrailingZero
+          | I64CountNonZero
+          | I64Add
+          | I64Sub
+          | I64Mul
+          | I64DivSign
+          | I64DivUnsign
+          | I64RemSign
+          | I64RemUnsign
+          | I64And
+          | I64Or
+          | I64Xor
+          | I64ShiftLeft
+          | I64ShiftRightSign
+          | I64ShiftRightUnsign
+          | I64RotateLeft
+          | I64RotateRight
+          | I64EqualZero
+          | I64Equal
+          | I64NotEqual
+          | I64LessThanSign
+          | I64LessThanUnSign
+          | I64GreaterThanSign
+          | I64GreaterThanUnSign
+          | I64LessEqualSign
+          | I64LessEqualUnSign
+          | I64GreaterEqualSign
+          | I64GreaterEqualUnSign
+          | I64Load(_, _)
+          | I64Load8Sign(_, _)
+          | I64Load8Unsign(_, _)
+          | I64Load16Sign(_, _)
+          | I64Load16Unsign(_, _)
+          | I64Load32Sign(_, _)
+          | I64Load32Unsign(_, _)
+          | I64ExtendSignI32
+          | I64ExtendUnsignI32
+          | I64TruncSignF32
+          | I64TruncUnsignF32
+          | I64TruncSignF64
+          | I64TruncUnsignF64
+          | I64ReinterpretF64 => return_types.push(ValueTypes::I64),
+          F32Const(_)
+          | F32Equal
+          | F32NotEqual
+          | F32LessThan
+          | F32GreaterThan
+          | F32LessEqual
+          | F32GreaterEqual
+          | F32Abs
+          | F32Neg
+          | F32Ceil
+          | F32Floor
+          | F32Trunc
+          | F32Nearest
+          | F32Sqrt
+          | F32Add
+          | F32Sub
+          | F32Mul
+          | F32Div
+          | F32Min
+          | F32Max
+          | F32Copysign
+          | F32Load(_, _)
+          | F32ConvertSignI32
+          | F32ConvertUnsignI32
+          | F32ConvertSignI64
+          | F32ConvertUnsignI64
+          | F32DemoteF64
+          | F32ReinterpretI32 => return_types.push(ValueTypes::F32),
+          F64Const(_)
+          | F64Equal
+          | F64NotEqual
+          | F64LessThan
+          | F64GreaterThan
+          | F64LessEqual
+          | F64GreaterEqual
+          | F64Abs
+          | F64Neg
+          | F64Ceil
+          | F64Floor
+          | F64Trunc
+          | F64Nearest
+          | F64Sqrt
+          | F64Add
+          | F64Sub
+          | F64Mul
+          | F64Div
+          | F64Min
+          | F64Max
+          | F64Copysign
+          | F64Load(_, _)
+          | F64ConvertSignI32
+          | F64ConvertUnsignI32
+          | F64ConvertSignI64
+          | F64ConvertUnsignI64
+          | F64PromoteF32
+          | F64ReinterpretI64 => return_types.push(ValueTypes::F64),
+          Nop
+          | DropInst
+          | SetLocal(_)
+          | SetGlobal(_)
+          | I32Store(_, _)
+          | I64Store(_, _)
+          | F32Store(_, _)
+          | F64Store(_, _)
+          | I32Store8(_, _)
+          | I32Store16(_, _)
+          | I64Store8(_, _)
+          | I64Store16(_, _)
+          | I64Store32(_, _) => {
+            unimplemented!(
+              "When enter this pattern, it's may time to implements case for not return anything."
+            );
+          }
+          // NOTE: Returns polymophic type
+          // Unreachable,
+          // Block(u32),
+          // Loop,
+          // If(u32, u32),
+          // Br(u32),
+          // BrIf(u32),
+          // BrTable(Vec<u32>, u32),
+          // Return,
+          // Call(usize), // FIXME: Change to u32
+          // CallIndirect(u32),
+          // GetLocal(u32),
+          // TeeLocal(u32),
+          // GetGlobal(u32),
+          // Select,
+          RuntimeValue(_) | Else | End => unimplemented!("This type do not produce any types."),
+          _ => unimplemented!(),
         };
       };
       inst_ptr += 1;
