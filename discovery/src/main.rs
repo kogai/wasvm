28150a71e9525064d8e94c65adbf5f6305aed42f
Shinichi Kogai <kogai0121@gmail.com>
2019-02-06 23:02:25 +0900

    :tada: Execute wasvm on actual microcontroller

--- discovery/src/main.rs
+++ discovery/src/main.rs
@@ -26,26 +26,19 @@ static ALLOCATOR: CortexMHeap = CortexMHeap::empty();
 #[entry]
 fn main() -> ! {
     let start = heap_start() as usize;
-    let size = 1024;
+    let size = 65536;
     unsafe { ALLOCATOR.init(start, size) }
 
-    hprintln!("Start VM").unwrap();
-
-    // add.wasm
-    let bytes = [
-        0x61, 0x00, 0x6d, 0x73, 0x00, 0x01, 0x00, 0x00, 0x07, 0x01, 0x60, 0x01, 0x7f, 0x02, 0x01,
-        0x7f, 0x03, 0x7f, 0x01, 0x02, 0x07, 0x00, 0x01, 0x0c, 0x5f, 0x08, 0x75, 0x73, 0x6a, 0x62,
-        0x63, 0x65, 0x00, 0x74, 0x0a, 0x00, 0x01, 0x09, 0x00, 0x07, 0x01, 0x20, 0x00, 0x20, 0x0b,
-        0x6a,
-    ];
+    let bytes = include_bytes!("add.wasm");
     let store = init_store();
-    let section = decode_module(&bytes);
+    let section = decode_module(bytes);
     let external_modules = ExternalModules::default();
-    let mut vm = instantiate_module(store, section, external_modules).unwrap();
+    let instance = instantiate_module(store, section, external_modules);
+    let mut vm = instance.unwrap();
     let result = vm.run("_subject", [Values::I32(10), Values::I32(20)].to_vec());
 
     // Need to enable semihosting of gdb
-    hprintln!("Result={}", result).unwrap();
+    hprintln!("Result={:?}", result).unwrap();
 
     loop {}
 }
