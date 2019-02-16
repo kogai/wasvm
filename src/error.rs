0b0c866a33890a5fe54a41fa3c6939c8899df3a4
Shinichi Kogai <kogai0121@gmail.com>
2019-02-16 14:08:13 +0900

    :recycle: No longer to provide error message as String

--- src/error.rs
+++ src/error.rs
@@ -1,11 +1,7 @@
-use alloc::string::String;
 use core::convert::From;
 use core::option::NoneError;
 
 pub mod runtime {
-  #[cfg(not(test))]
-  use alloc::prelude::*;
-  use alloc::string::String;
   use core::convert::From;
   use core::option::NoneError;
 
@@ -55,52 +51,9 @@ pub mod runtime {
       Trap::UnexpectedEnd
     }
   }
-
-  impl From<Trap> for String {
-    fn from(x: Trap) -> Self {
-      use self::Trap::*;
-      match x {
-        DivisionOverflow => "integer overflow",
-        DivisionByZero => "integer divide by zero",
-        DataSegmentDoesNotFit => "data segment does not fit",
-        ElementSegmentDoesNotFit => "elements segment does not fit",
-        MemoryAccessOutOfBounds => "out of bounds memory access",
-        BitshiftOverflow => "bit shift overflow",
-        IntegerOverflow => "integer overflow",
-        Unknown => "unknown",
-        Undefined => "undefined behavior occurred",
-        UndefinedElement => "undefined element",
-        Notfound => "not found",
-        StackOverflow => "stack overflow",
-        StackUnderflow => "stack underflow",
-        TypeMismatch => "type mismatch",
-        IndirectCallTypeMismatch => "indirect call type mismatch",
-        InvalidMutability => "invalid mutability",
-        FailToGrow => "fail to grow",
-        InvalidConversionToInt => "invalid conversion to integer",
-        UnexpectedEnd => "unexpected end",
-        InvalidSectionId => "invalid section id",
-        LengthOutofBounds => "length out of bounds",
-        Unreachable => "unreachable executed",
-        UnknownImport => "unknown import",
-        IncompatibleImportType => "incompatible import type",
-        UninitializedElement => "uninitialized element",
-        UnsupportedTextform => "unsupported text form",
-        MagicHeaderNotDetected => "magic header not detected",
-        IntegerRepresentationTooLong => "integer representation too long",
-        FunctionAndCodeInconsitent => "function and code section have inconsistent lengths",
-        InvalidUTF8Encoding => "invalid UTF-8 encoding",
-        LinearMapOverflowed => "LinearMap has been overflowed",
-      }
-      .to_owned()
-    }
-  }
 }
 
 pub mod validate_time {
-  #[cfg(not(test))]
-  use alloc::prelude::*;
-  use alloc::string::String;
   use core::convert::From;
   use core::option::NoneError;
 
@@ -137,34 +90,6 @@ pub mod validate_time {
   impl From<NoneError> for TypeError {
     fn from(_: NoneError) -> Self {
       TypeError::NotFound
-    }
-  }
-
-  impl From<TypeError> for String {
-    fn from(x: TypeError) -> Self {
-      use self::TypeError::*;
-      match x {
-        NotFound => "not found".to_string(),
-        MultipleTables => "multiple tables".to_string(),
-        MultipleMemories => "multiple memories".to_string(),
-        TypeMismatch => "type mismatch".to_string(),
-        IndirectCallTypeMismatch => "indirect call type mismatch".to_string(),
-        IncompatibleImportType => "incompatible import type".to_string(),
-        InvalidResultArity => "invalid result arity".to_string(),
-        InvalidAlignment => "alignment must not be larger than natural".to_string(),
-        InvalidMemorySize => "invalid memory size".to_string(),
-        InvalidStartFunction => "invalid start function".to_string(),
-        UnknownLabel => "unknown label".to_string(),
-        UnknownLocal => "unknown local".to_string(),
-        UnknownMemory => "unknown memory 0".to_string(),
-        ConstantExpressionRequired => "constant expression required".to_string(),
-        UnknownFunction(idx) => format!("unknown function {}", idx),
-        UnknownFunctionType(idx) => format!("unknown function type {}", idx),
-        UnknownTable(idx) => format!("unknown table {}", idx),
-        UnknownGlobal(idx) => format!("unknown global {}", idx),
-        DuplicateExportName => "duplicate export name".to_string(),
-        GlobalIsImmutable => "global is immutable".to_string(),
-      }
     }
   }
 }
@@ -214,15 +139,6 @@ impl From<WasmError> for self::validate_time::TypeError {
 impl From<self::runtime::Trap> for WasmError {
   fn from(trap: self::runtime::Trap) -> Self {
     WasmError::Trap(trap)
-  }
-}
-
-impl From<WasmError> for String {
-  fn from(x: WasmError) -> Self {
-    match x {
-      WasmError::Trap(e) => String::from(e),
-      WasmError::TypeError(e) => String::from(e),
-    }
   }
 }
 
